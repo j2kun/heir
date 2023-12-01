9ae78f7c1387b09017423c195354daa706c31ad6
Jeremy Kun <j2kun@users.noreply.github.com>
2023-11-30 22:54:55 -0800

    start generalizing distribute-generic

--- lib/Dialect/Secret/Transforms/DistributeGeneric.cpp
+++ lib/Dialect/Secret/Transforms/DistributeGeneric.cpp
@@ -5,6 +5,8 @@
 
 #include "include/Dialect/Secret/IR/SecretOps.h"
 #include "include/Dialect/Secret/IR/SecretTypes.h"
+#include "llvm/include/llvm/Support/Casting.h"        // from @llvm-project
+#include "llvm/include/llvm/Support/Debug.h"          // from @llvm-project
 #include "llvm/include/llvm/Support/ErrorHandling.h"  // from @llvm-project
 #include "mlir/include/mlir/IR/Block.h"               // from @llvm-project
 #include "mlir/include/mlir/IR/Builders.h"            // from @llvm-project
@@ -15,9 +17,12 @@
 #include "mlir/include/mlir/IR/Types.h"               // from @llvm-project
 #include "mlir/include/mlir/IR/Value.h"               // from @llvm-project
 #include "mlir/include/mlir/IR/ValueRange.h"          // from @llvm-project
+#include "mlir/include/mlir/Interfaces/LoopLikeInterface.h"  // from @llvm-project
 #include "mlir/include/mlir/Support/LLVM.h"           // from @llvm-project
 #include "mlir/include/mlir/Support/LogicalResult.h"  // from @llvm-project
 #include "mlir/include/mlir/Transforms/GreedyPatternRewriteDriver.h"  // from @llvm-project
+
+#define DEBUG_TYPE "secret.distribute-generic"
 
 namespace mlir {
 namespace heir {
@@ -25,6 +30,15 @@ namespace secret {
 
 #define GEN_PASS_DEF_SECRETDISTRIBUTEGENERIC
 #include "include/Dialect/Secret/Transforms/Passes.h.inc"
+
+std::optional<Value> ofrToValue(std::optional<OpFoldResult> ofr) {
+  if (ofr.has_value()) {
+    if (auto value = llvm::dyn_cast_if_present<Value>(*ofr)) {
+      return value;
+    }
+  }
+  return std::nullopt;
+}
 
 // Inline the inner block of a secret.generic that has no secret operands.
 //
@@ -44,7 +58,7 @@ namespace secret {
 //
 struct CollapseSecretlessGeneric : public OpRewritePattern<GenericOp> {
   CollapseSecretlessGeneric(mlir::MLIRContext *context)
-      : OpRewritePattern<GenericOp>(context, /*benefit=*/2) {}
+      : OpRewritePattern<GenericOp>(context, /*benefit=*/3) {}
 
   LogicalResult matchAndRewrite(GenericOp op,
                                 PatternRewriter &rewriter) const override {
@@ -85,7 +99,7 @@ struct CollapseSecretlessGeneric : public OpRewritePattern<GenericOp> {
 //
 struct RemoveUnusedGenericArgs : public OpRewritePattern<GenericOp> {
   RemoveUnusedGenericArgs(mlir::MLIRContext *context)
-      : OpRewritePattern<GenericOp>(context, /*benefit=*/1) {}
+      : OpRewritePattern<GenericOp>(context, /*benefit=*/2) {}
 
   LogicalResult matchAndRewrite(GenericOp op,
                                 PatternRewriter &rewriter) const override {
@@ -108,8 +122,7 @@ struct RemoveUnusedGenericArgs : public OpRewritePattern<GenericOp> {
   }
 };
 
-// Split a secret.generic containing multiple ops into its underlying op if no
-// secret values are included in its input.
+// Split a secret.generic containing multiple ops into multiple secret.generics.
 //
 // E.g.,
 //
@@ -133,36 +146,137 @@ struct RemoveUnusedGenericArgs : public OpRewritePattern<GenericOp> {
 //      secret.yield %7 : i32
 //    } -> !secret.secret<i32>
 //
-struct PeelFromGeneric : public OpRewritePattern<GenericOp> {
-  PeelFromGeneric(mlir::MLIRContext *context)
-      : OpRewritePattern<GenericOp>(context, /*benefit=*/1) {}
+// When options are provided specifying which ops to distribute, the pattern
+// will split at the first detected specified op, possibly creating three new
+// secret.generics, and otherwise will split it at the first op from the entry
+// block, and will always create two secret.generics.
+struct SplitGeneric : public OpRewritePattern<GenericOp> {
+  SplitGeneric(mlir::MLIRContext *context,
+               llvm::ArrayRef<std::string> opsToDistribute)
+      : OpRewritePattern<GenericOp>(context, /*benefit=*/1),
+        opsToDistribute(opsToDistribute) {}
 
-  LogicalResult matchAndRewrite(GenericOp op,
-                                PatternRewriter &rewriter) const override {
+  void distributeThroughRegionHoldingOp(GenericOp genericOp,
+                                        Operation &opToDistribute,
+                                        PatternRewriter &rewriter) const {
+    assert(opToDistribute.getNumRegions() > 0 &&
+           "opToDistribute must have at least one region");
+    assert(genericOp.getBody()->getOperations().size() == 2 &&
+           "opToDistribute must have one non-yield op");
+
+    // Supports ops with countable loop iterations, like affine.for and scf.for,
+    // but not scf.while which has multiple associated regions.
+    if (auto loop = dyn_cast<LoopLikeOpInterface>(opToDistribute)) {
+      // FIXME: delete, not needed because these come from block arguments of
+      // the generic which are never secret.
+      //
+      // auto iv = loop.getSingleInductionVar();
+      // auto lowerBound = ofrToValue(loop.getSingleLowerBound());
+      // auto upperBound = ofrToValue(loop.getSingleUpperBound());
+      // auto step = ofrToValue(loop.getSingleStep());
+
+      // if ((iv && isa<SecretType>(iv->getType())) ||
+      //     (lowerBound && isa<SecretType>(lowerBound->getType())) ||
+      //     (upperBound && isa<SecretType>(upperBound->getType())) ||
+      //     (step && isa<SecretType>(step->getType()))) {
+      //   opToDistribute.emitOpError()
+      //       << "Found loop-like op with induction variable, step, or bounds
+      //       of "
+      //          "type secret.sercret.\n";
+      // }
+
+      // Example:
+      //
+      //   secret.generic ins(%value : !secret.secret<...>) {
+      //   ^bb0(%clear_value: ...):
+      //     %1 = scf.for ... iter_args(%iter_arg = %clear_value) -> ... {
+      //       scf.yield ...
+      //     }
+      //     secret.yield %1 : ...
+      //   } -> (!secret.secret<...>)
+      //
+      // This needs to be converted to:
+      //
+      //   %1 = scf.for ... iter_args(%iter_arg = %value) -> ... {
+      //     %2 = secret.generic ins(%iter_arg : !secret.secret<...>) {
+      //     ^bb0(%clear_iter_arg: ...):
+      //       ...
+      //       secret.yield %1 : ...
+      //     }
+      //     scf.yield %2 : ...
+      //   }
+      //
+      // Terminators of the region are not part of the secret, since they just
+      // handle control flow.
+
+      // Before moving the loop out of the generic, connect the loop's operands
+      // to the corresponding secret operands (via the block argument number).
+      genericOp->getParentOp()->dump();
+      rewriter.startRootUpdate(genericOp);
+
+      // Set the op's operands
+      MutableArrayRef<OpOperand> operands = opToDistribute.getOpOperands();
+      for (OpOperand &operand : operands) {
+        assert(
+            isa<BlockArgument>(operand.get()) &&
+            "loop init values must be block arguments of the secret.generic");
+        BlockArgument initArg = cast<BlockArgument>(operand.get());
+        operand.set(genericOp.getOperand(initArg.getArgNumber()));
+      }
+
+      // Set the op's region iter arg types, which need to match the possibly
+      // new type of the operands modified above
+      for (auto [arg, operand] :
+           llvm::zip(loop.getRegionIterArgs(), loop.getInits())) {
+        arg.setType(operand.getType());
+      }
+
+      opToDistribute.moveBefore(genericOp);
+
+      // Now the loop is before the secret generic, but the generic still
+      // yields the loop's result (the loop should yield the generic's result)
+      // and the generic still needs to be moved inside the loop.
+
+      genericOp->moveBefore(&loop.getLoopRegions()
+                                 .front()
+                                 ->getBlocks()
+                                 .front()
+                                 .getOperations()
+                                 .front());
+
+      auto yieldedValues = loop.getYieldedValues();
+      auto *terminator = opToDistribute.getRegion(0).front().getTerminator();
+
+      rewriter.finalizeRootUpdate(genericOp);
+      genericOp->getParentOp()->dump();
+
+      assert(false && "wat");
+
+      return;
+    }
+
+    // Cast to loop-like interface to assert the induction variables and bounds
+    // are not secret, but iter-args may be.
+
+    // TODO: handle RegionBranchOpInterface (scf.while, scf.if) and potentially
+    // BranchOpInterface (cf.br)
+  }
+
+  void splitGenericAroundOp(GenericOp op, Operation &opToDistribute,
+                            PatternRewriter &rewriter) const {
+    assert(false && "not implemented");
+  }
+
+  void splitGenericAfterOp(GenericOp op, Operation &opToDistribute,
+                           PatternRewriter &rewriter) const {
     Block *body = op.getBody();
-    unsigned numOps = body->getOperations().size();
-    if (numOps == 0) {
-      llvm_unreachable(
-          "secret.generic must have nonempty body (the verifier should "
-          "enforce "
-          "this)");
-    }
-
-    // Recursive base case: stop if there's only one op left, noting that we
-    // check for 2 ops because the last op is enforced to be a yield op by the
-    // verifier.
-    if (numOps == 1 || numOps == 2) {
-      return failure();
-    }
-
-    Operation &firstOp = body->getOperations().front();
     // The inputs to the op are generic op's block arguments (cleartext
     // values), and they need to change to be the corresponding generic op's
     // normal operands (maybe secret values).
     SmallVector<Value> newInputs;
     // The indices of the new inputs in the original block argument list
     SmallVector<unsigned> newInputIndices;
-    for (Value val : firstOp.getOperands()) {
+    for (Value val : opToDistribute.getOperands()) {
       int index = std::find(body->getArguments().begin(),
                             body->getArguments().end(), val) -
                   body->getArguments().begin();
@@ -172,34 +286,85 @@ struct PeelFromGeneric : public OpRewritePattern<GenericOp> {
 
     // Result types are secret versions of the results of the block's only op
     SmallVector<Type> newResultTypes;
-    for (Type ty : firstOp.getResultTypes()) {
+    for (Type ty : opToDistribute.getResultTypes()) {
       newResultTypes.push_back(SecretType::get(ty));
     }
 
     auto newGeneric = rewriter.create<GenericOp>(
         op.getLoc(), newInputs, newResultTypes,
         [&](OpBuilder &b, Location loc, ValueRange blockArguments) {
-          auto *newOp = b.clone(firstOp);
+          auto *newOp = b.clone(opToDistribute);
           newOp->setOperands(blockArguments);
           b.create<YieldOp>(loc, newOp->getResults());
         });
 
     // Once the op is split off into a new generic op, we need to add new
     // operands to the old generic op, add new corresponding block arguments,
-    // and replace all uses of the firstOp's results with the created block
-    // arguments.
+    // and replace all uses of the opToDistribute's results with the created
+    // block arguments.
     SmallVector<Value> oldGenericNewBlockArgs;
     rewriter.updateRootInPlace(op, [&]() {
       op.getInputsMutable().append(newGeneric.getResults());
-      for (auto ty : firstOp.getResultTypes()) {
-        BlockArgument arg = op.getBody()->addArgument(ty, firstOp.getLoc());
+      for (auto ty : opToDistribute.getResultTypes()) {
+        BlockArgument arg =
+            op.getBody()->addArgument(ty, opToDistribute.getLoc());
         oldGenericNewBlockArgs.push_back(arg);
       }
     });
-    rewriter.replaceOp(&firstOp, oldGenericNewBlockArgs);
+    rewriter.replaceOp(&opToDistribute, oldGenericNewBlockArgs);
+  }
 
+  LogicalResult matchAndRewrite(GenericOp op,
+                                PatternRewriter &rewriter) const override {
+    Block *body = op.getBody();
+    unsigned numOps = body->getOperations().size();
+    assert(numOps > 0 &&
+           "secret.generic must have nonempty body (the verifier should "
+           "enforce this)");
+
+    // Recursive base case: stop if there's only one op left, and it has no
+    // regions, noting that we check for 2 ops because the last op is enforced
+    // to be a yield op by the verifier.
+    if (numOps == 2 && body->front().getRegions().empty()) {
+      return failure();
+    }
+
+    Operation *opToDistribute;
+    bool first = true;
+    if (opsToDistribute.empty()) {
+      opToDistribute = &body->front();
+    } else {
+      for (Operation &op : body->getOperations()) {
+        // op.getName().getStringRef() is the qualified op name (e.g.,
+        // affine.for)
+        if (std::find(opsToDistribute.begin(), opsToDistribute.end(),
+                      op.getName().getStringRef()) != opsToDistribute.end()) {
+          opToDistribute = &op;
+          break;
+        }
+        first = false;
+      }
+    }
+
+    // Base case: if none of a generic op's member ops are in the list of ops
+    // to process, stop.
+    if (!opToDistribute) return failure();
+
+    if (numOps == 2 && !opToDistribute->getRegions().empty()) {
+      distributeThroughRegionHoldingOp(op, *opToDistribute, rewriter);
+      return success();
+    }
+
+    if (first) {
+      splitGenericAfterOp(op, *opToDistribute, rewriter);
+    } else {
+      splitGenericAroundOp(op, *opToDistribute, rewriter);
+    }
     return success();
   }
+
+ private:
+  llvm::ArrayRef<std::string> opsToDistribute;
 };
 
 struct DistributeGeneric
@@ -209,8 +374,21 @@ struct DistributeGeneric
   void runOnOperation() override {
     MLIRContext *context = &getContext();
     mlir::RewritePatternSet patterns(context);
-    patterns.add<PeelFromGeneric, CollapseSecretlessGeneric,
-                 RemoveUnusedGenericArgs>(context);
+
+    LLVM_DEBUG({
+      llvm::dbgs() << "Running secret-distribute-generic ";
+      if (opsToDistribute.empty()) {
+        llvm::dbgs() << "on all ops\n";
+      } else {
+        llvm::dbgs() << "on ops: \n";
+        for (const auto &op : opsToDistribute) {
+          llvm::dbgs() << " - " << op << "\n";
+        }
+      }
+    });
+
+    patterns.add<SplitGeneric>(context, opsToDistribute);
+    patterns.add<CollapseSecretlessGeneric, RemoveUnusedGenericArgs>(context);
     // TODO(https://github.com/google/heir/issues/170): add a pattern that
     // distributes generic through a single op containing one or more regions.
     (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
--- tests/secret/distribute_generic.mlir
+++ tests/secret/distribute_generic.mlir
@@ -51,17 +51,40 @@ func.func @test_distribute_generic(%value: !secret.secret<i32>, %cond: i1) -> !s
 
 // CHECK-LABEL: test_affine_for
 // CHECK-SAME: %[[value:.*]]: !secret.secret<i32>) {
-func.func @test_affine_for(%value: !secret.secret<i32>) {
-  secret.generic
+func.func @test_affine_for(%value: !secret.secret<i32>) -> !secret.secret<i32> {
+  // CHECK-DAG: %[[c1i32:.*]] = arith.constant 1 : i32
+  // CHECK-DAG: %[[c1:.*]] = arith.constant 1 : index
+  // CHECK-DAG: %[[c5:.*]] = arith.constant 5 : index
+
+  // The for loop takes the secret as iter_arg and returns it
+  // CHECK-NEXT: %[[v0:.*]] = scf.for %[[i:.*]] = %[[c1]] to %[[c5]] step %[[c1]]
+  // CHECK-SAME: iter_args(%[[iter_arg:.*]] = %[[value]]) -> !secret.secret<i32> {
+
+  // The loop body is a single secret.generic for the add
+  // CHECK-NEXT: %[[g0:.*]] = secret.generic ins(%[[iter_arg]], %[[c1i32]] : !secret.secret<i32>, i32) {
+  // CHECK-NEXT: ^[[bb0:.*]](%[[clear_iter_arg:.*]]: i32, %[[clear_c1i32:.*]]: i32):
+  // CHECK-NEXT:   %[[g0_op:.*]] = arith.addi %[[clear_iter_arg]], %[[clear_c1i32]] : i32
+  // CHECK-NEXT:   secret.yield %[[g0_op]] : i32
+  // CHECK-NEXT: } -> !secret.secret<i32>
+
+  // Terminators are not part of a generic.
+  // CHECK-NEXT: scf.yield %[[g0]] : !secret.secret<i32>
+  // CHECK-NEXT: }
+
+  // CHECK-NEXT: func.return %[[v0]]
+  %Z = secret.generic
     ins(%value : !secret.secret<i32>) {
     ^bb0(%clear_value: i32):
-      affine.for %i = 0 to 10 {
-        %c1 = arith.constant 1 : i32
-        %0 = arith.addi %clear_value, %c1 : i32
+      %0 = arith.constant 1 : i32
+      %c1 = arith.constant 1 : index
+      %c5 = arith.constant 5 : index
+      %1 = scf.for %i = %c1 to %c5 step %c1 iter_args(%iter_arg = %clear_value) -> i32 {
+        %2 = arith.addi %iter_arg, %0 : i32
+        scf.yield %2 : i32
       }
-      secret.yield
-    } -> ()
-  func.return
+      secret.yield %1 : i32
+    } -> (!secret.secret<i32>)
+  func.return %Z : !secret.secret<i32>
 }
 
 
