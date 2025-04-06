2bea77e541b5f7ca870c0ec5c17ddaf502c96477
Jeremy Kun <j2kun@users.noreply.github.com>
2025-04-06 11:30:59 -0700

    move type conversion helpers to their own lib for reuse

--- lib/Transforms/ConvertToCiphertextSemantics/ConvertToCiphertextSemantics.cpp
+++ lib/Transforms/ConvertToCiphertextSemantics/ConvertToCiphertextSemantics.cpp
@@ -1,6 +1,5 @@
 #include "lib/Transforms/ConvertToCiphertextSemantics/ConvertToCiphertextSemantics.h"
 
-#include <algorithm>
 #include <cmath>
 #include <cstdint>
 #include <numeric>
@@ -15,6 +14,7 @@
 #include "lib/Dialect/TensorExt/IR/TensorExtAttributes.h"
 #include "lib/Dialect/TensorExt/IR/TensorExtDialect.h"
 #include "lib/Dialect/TensorExt/IR/TensorExtOps.h"
+#include "lib/Transforms/ConvertToCiphertextSemantics/TypeConversion.h"
 #include "lib/Utils/AffineMapUtils.h"
 #include "lib/Utils/AttributeUtils.h"
 #include "lib/Utils/ContextAwareConversionUtils.h"
@@ -94,8 +94,7 @@ struct LayoutMaterializationTypeConverter
     : public UniquelyNamedAttributeAwareTypeConverter {
  public:
   LayoutMaterializationTypeConverter(int ciphertextSize)
-      : UniquelyNamedAttributeAwareTypeConverter(kLayoutAttrName),
-        ciphertextSize(ciphertextSize) {
+      : UniquelyNamedAttributeAwareTypeConverter(kLayoutAttrName) {
     addConversion([&](Type type, Attribute attr) { return std::nullopt; });
     addConversion(
         [&](secret::SecretType type, LayoutAttr attr) -> std::optional<Type> {
@@ -103,9 +102,11 @@ struct LayoutMaterializationTypeConverter
           auto innerType = type.getValueType();
 
           if (auto rankedTensorType = dyn_cast<RankedTensorType>(innerType)) {
-            convertedInnerType = materializeLayout(rankedTensorType, attr);
+            convertedInnerType =
+                materializeLayout(rankedTensorType, attr, ciphertextSize);
           } else {
-            convertedInnerType = materializeScalarLayout(innerType, attr);
+            convertedInnerType =
+                materializeScalarLayout(innerType, attr, ciphertextSize);
           }
 
           if (failed(convertedInnerType)) return std::nullopt;
@@ -113,69 +114,16 @@ struct LayoutMaterializationTypeConverter
         });
     addConversion(
         [&](RankedTensorType type, LayoutAttr attr) -> std::optional<Type> {
-          return materializeLayout(type, attr);
+          return materializeLayout(type, attr, ciphertextSize);
         });
     addConversion(
         [&](IntegerType type, LayoutAttr attr) -> std::optional<Type> {
-          return materializeScalarLayout(type, attr);
+          return materializeScalarLayout(type, attr, ciphertextSize);
         });
     addConversion([&](FloatType type, LayoutAttr attr) -> std::optional<Type> {
-      return materializeScalarLayout(type, attr);
+      return materializeScalarLayout(type, attr, ciphertextSize);
     });
   }
-
-  FailureOr<Type> materializeScalarLayout(Type type, LayoutAttr attr) const {
-    // TODO(#1662): improve scalar layout materialization
-    // Support scalars with non-repetition layouts (e.g., in slot 0 with
-    // 0-padding); currently the layout system always produces a pure-repeition
-    // alignment and a trivial row-major layout.
-    return RankedTensorType::get({ciphertextSize}, type);
-  }
-
-  FailureOr<Type> materializeLayout(RankedTensorType type,
-                                    LayoutAttr attr) const {
-    AffineMap layout = attr.getMap();
-    LLVM_DEBUG(llvm::dbgs() << "Unaligned type: " << type << "\n");
-
-    // First extract the tensor type as expanded according to the
-    // alignment attribute.
-    tensor_ext::AlignmentAttr alignment = attr.getAlignment();
-    if (alignment) {
-      type = RankedTensorType::get(alignment.getOut(), type.getElementType());
-    }
-    LLVM_DEBUG(llvm::dbgs() << "Aligned type: " << type << "\n");
-
-    // Each ciphertext will always have ciphertextSize many slots, so the main
-    // goal is to determine how many ciphertexts are needed. We do this by
-    // iterating over the input type's index domain, and apply the layout
-    // affine map to each index, and keep track of the maximum value of each
-    // index of the map results. These maxima (plus 1 for zero indexing)
-    // will be the shape of the new type.
-    SmallVector<int64_t> outputTensorShape(layout.getNumResults(), 0);
-    outputTensorShape[layout.getNumResults() - 1] = ciphertextSize;
-
-    // Evaluate the affine map on the input indices and update the
-    // outputTensorShape to be a max over visited indices.
-    IndexTupleConsumer evaluateNextIndex =
-        [&](const std::vector<int64_t> &indices) {
-          SmallVector<int64_t> results;
-          evaluateStatic(layout, indices, results);
-
-          // minus 1 to skip the last dimension (ciphertext dimension)
-          for (int i = 0; i < layout.getNumResults() - 1; ++i) {
-            // 1 + to account for zero indexing
-            outputTensorShape[i] =
-                std::max(outputTensorShape[i], 1 + results[i]);
-          }
-        };
-
-    iterateIndices(type.getShape(), evaluateNextIndex);
-    return RankedTensorType::get(outputTensorShape, type.getElementType());
-  }
-
- private:
-  // The number of slots available in each ciphertext.
-  int ciphertextSize;
 };
 
 bool hasMaterializedAttr(Operation *op) {
