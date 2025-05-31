4afa1c4efd878e92af612ba6620c25fbd8eccade
Jeremy Kun <j2kun@users.noreply.github.com>
2025-05-30 17:16:50 -0700

    start adding secret-insert-mgmt-plaintext

---
+++ lib/Transforms/SecretInsertMgmt/SecretInsertMgmtPlaintext.cpp
@@ -0,0 +1,125 @@
+#include <cstdint>
+#include <iterator>
+#include <utility>
+
+#include "lib/Analysis/LevelAnalysis/LevelAnalysis.h"
+#include "lib/Analysis/MulDepthAnalysis/MulDepthAnalysis.h"
+#include "lib/Analysis/SecretnessAnalysis/SecretnessAnalysis.h"
+#include "lib/Dialect/Mgmt/IR/MgmtOps.h"
+#include "lib/Dialect/Mgmt/Transforms/AnnotateMgmt.h"
+#include "lib/Dialect/Mgmt/Transforms/Passes.h"
+#include "lib/Dialect/ModuleAttributes.h"
+#include "lib/Dialect/Secret/IR/SecretOps.h"
+#include "lib/Transforms/SecretInsertMgmt/Passes.h"
+#include "lib/Transforms/SecretInsertMgmt/SecretInsertMgmtPatterns.h"
+#include "lib/Utils/Utils.h"
+#include "llvm/include/llvm/ADT/STLExtras.h"   // from @llvm-project
+#include "llvm/include/llvm/ADT/TypeSwitch.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlow/ConstantPropagationAnalysis.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlow/DeadCodeAnalysis.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlowFramework.h"  // from @llvm-project
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"      // from @llvm-project
+#include "mlir/include/mlir/Dialect/Tensor/IR/Tensor.h"    // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinAttributes.h"        // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinTypes.h"             // from @llvm-project
+#include "mlir/include/mlir/IR/Diagnostics.h"              // from @llvm-project
+#include "mlir/include/mlir/IR/Operation.h"                // from @llvm-project
+#include "mlir/include/mlir/IR/PatternMatch.h"             // from @llvm-project
+#include "mlir/include/mlir/Pass/PassManager.h"            // from @llvm-project
+#include "mlir/include/mlir/Support/LLVM.h"                // from @llvm-project
+#include "mlir/include/mlir/Transforms/Passes.h"           // from @llvm-project
+#include "mlir/include/mlir/Transforms/WalkPatternRewriteDriver.h"  // from @llvm-project
+
+namespace mlir {
+namespace heir {
+
+#define GEN_PASS_DEF_SECRETINSERTMGMTPLAINTEXT
+#include "lib/Transforms/SecretInsertMgmt/Passes.h.inc"
+
+struct SecretInsertMgmtPlaintext
+    : impl::SecretInsertMgmtPlaintextBase<SecretInsertMgmtPlaintext> {
+  using SecretInsertMgmtPlaintextBase::SecretInsertMgmtPlaintextBase;
+
+  void runOnOperation() override {
+    DataFlowSolver solver;
+    solver.load<dataflow::DeadCodeAnalysis>();
+    solver.load<dataflow::SparseConstantPropagation>();
+    solver.load<SecretnessAnalysis>();
+    solver.load<LevelAnalysis>();
+
+    if (failed(solver.initializeAndRun(getOperation()))) {
+      getOperation()->emitOpError() << "Failed to run the analysis.\n";
+      signalPassFailure();
+      return;
+    }
+
+    // Handle plaintext operands. In the plaintext backend, these are
+    // "cleartext" operands which must be encoded as plaintexts, so the name is
+    // confusing.
+    RewritePatternSet patternsPlaintext(&getContext());
+    patternsPlaintext.add<UseInitOpForPlaintextOperand<arith::AddIOp>,
+                          UseInitOpForPlaintextOperand<arith::SubIOp>,
+                          UseInitOpForPlaintextOperand<arith::MulIOp>,
+                          UseInitOpForPlaintextOperand<arith::AddFOp>,
+                          UseInitOpForPlaintextOperand<arith::SubFOp>,
+                          UseInitOpForPlaintextOperand<arith::MulFOp>>(
+        &getContext(), getOperation(), &solver);
+    (void)walkAndApplyPatterns(getOperation(), std::move(patternsPlaintext));
+
+    RewritePatternSet patternsMultModReduce(&getContext());
+    // tensor::ExtractOp = mulConst + rotate
+    patternsMultModReduce
+        .add<ModReduceBefore<arith::MulIOp>, ModReduceBefore<arith::MulFOp>,
+             ModReduceBefore<tensor::ExtractOp>>(
+            &getContext(), beforeMulIncludeFirstMul, getOperation(), &solver);
+    // includeFirstMul = false here
+    // as before yield we only want mulResult to be mod reduced
+    patternsMultModReduce.add<ModReduceBefore<secret::YieldOp>>(
+        &getContext(), /*includeFirstMul*/ false, getOperation(), &solver);
+    (void)walkAndApplyPatterns(getOperation(),
+                               std::move(patternsMultModReduce));
+
+    // when other binary op operands level mismatch
+    //
+    // See also MatchCrossLevel documentation
+    int idCounter = 0;  // for making adjust_scale op different to avoid cse
+    RewritePatternSet patternsAddModReduce(&getContext());
+    patternsAddModReduce
+        .add<MatchCrossLevel<arith::AddIOp>, MatchCrossLevel<arith::SubIOp>,
+             MatchCrossLevel<arith::MulIOp>, MatchCrossLevel<arith::AddFOp>,
+             MatchCrossLevel<arith::SubFOp>, MatchCrossLevel<arith::MulFOp>>(
+            &getContext(), &idCounter, getOperation(), &solver);
+    (void)walkAndApplyPatterns(getOperation(), std::move(patternsAddModReduce));
+
+    // when other binary op operands mulDepth mismatch
+    // this only happen for before-mul but not include-first-mul case
+    // at the first level, a Value can be both mulResult or not mulResult
+    // we should match their scale by adding one adjust scale op
+    //
+    // See also MatchCrossMulDepth documentation
+    if (!beforeMulIncludeFirstMul && !afterMul) {
+      RewritePatternSet patternsMulDepth(&getContext());
+      patternsMulDepth.add<
+          MatchCrossMulDepth<arith::MulIOp>, MatchCrossMulDepth<arith::AddIOp>,
+          MatchCrossMulDepth<arith::SubIOp>, MatchCrossMulDepth<arith::MulFOp>,
+          MatchCrossMulDepth<arith::AddFOp>, MatchCrossMulDepth<arith::SubFOp>>(
+          &getContext(), &idCounter, getOperation(), &solver);
+      (void)walkAndApplyPatterns(getOperation(), std::move(patternsMulDepth));
+    }
+
+    // call Canonicalizer here because mgmt ops need to be ordered
+    // call CSE here because there may be redundant mod reduce
+    // one Value may get mod reduced multiple times in
+    // multiple Uses
+    //
+    // also run annotate-mgmt for lowering
+    OpPassManager pipeline("builtin.module");
+    pipeline.addPass(createCanonicalizerPass());
+    pipeline.addPass(createCSEPass());
+    pipeline.addPass(mgmt::createAnnotateMgmt());
+    (void)runPipeline(pipeline, getOperation());
+  }
+};
+
+}  // namespace heir
+}  // namespace mlir
