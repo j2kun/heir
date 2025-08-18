4c30fa99b318b7e5a5c6e6e6435187e490dda28c
Jeremy Kun <j2kun@users.noreply.github.com>
2025-08-18 16:08:43 -0700

    checkpoint

--- lib/Utils/Layout/Hoisting.cpp
+++ lib/Utils/Layout/Hoisting.cpp
@@ -7,12 +7,51 @@ namespace mlir {
 namespace heir {
 
 using presburger::IntegerRelation;
+using presburger::PresburgerSpace;
+using presburger::VarKind;
 
 presburger::IntegerRelation hoistConversionThroughMatvec(
-    const presburger::IntegerRelation& matrixLayout,
-    const presburger::IntegerRelation& fromVecLayout,
-    const presburger::IntegerRelation& toVecLayout) {
-  return matrixLayout;
+    const IntegerRelation& matrixLayout, const IntegerRelation& fromVecLayout,
+    const IntegerRelation& toVecLayout) {
+  // The intuition for this function is that the conversion from fromVecLayout
+  // to toVecLayout implies some transformation of the slot ordering of the
+  // packed vector. The kernels we support have matrix layouts for which the
+  // packed slots of a ciphertext track the columns of the packed vector.
+  // So we need to apply the same transformation of the packed vector slots
+  // to the packed matrix slots.
+  //
+  // This function works in two steps:
+  //
+  // 1. Compute the inferred re-packing relation of vector slots (i.e., a
+  // relation (ct, slots) -> (ct, slots))
+  //
+  // 2. Compose the transformation from (1) with the (ct, slot) dims of the
+  // matrix packing.
+
+  // llvm::outs() << "fromVecLayout:\n";
+  // llvm::outs().flush();
+  // fromVecLayout.dump();
+  // llvm::outs() << "toVecLayout:\n";
+  // llvm::outs().flush();
+  // toVecLayout.dump();
+
+  IntegerRelation fromClone(fromVecLayout);
+  fromClone.inverse();
+  // llvm::outs() << "fromCloneInverse:\n";
+  // llvm::outs().flush();
+  // fromClone.dump();
+
+  fromClone.compose(toVecLayout);
+  fromClone.removeRedundantConstraints();
+  fromClone.simplify();
+
+  // llvm::outs() << "fromClone:\n";
+  // llvm::outs().flush();
+  // fromClone.dump();
+
+  IntegerRelation result(matrixLayout);
+  result.applyRange(fromClone);
+  return result;
 }
 
 }  // namespace heir
--- lib/Utils/Layout/HoistingTest.cpp
+++ lib/Utils/Layout/HoistingTest.cpp
@@ -1,18 +1,14 @@
-#include <cmath>
-#include <cstdint>
 #include <optional>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"  // from @googletest
+#include "lib/Utils/Layout/Codegen.h"
 #include "lib/Utils/Layout/Hoisting.h"
 #include "lib/Utils/Layout/Parser.h"
-#include "lib/Utils/Layout/Utils.h"
-#include "lib/Utils/TensorUtils.h"
-#include "llvm/include/llvm/ADT/SmallVector.h"  // from @llvm-project
 #include "mlir/include/mlir/Analysis/Presburger/IntegerRelation.h"  // from @llvm-project
 #include "mlir/include/mlir/Analysis/Presburger/PresburgerSpace.h"  // from @llvm-project
-#include "mlir/include/mlir/IR/BuiltinTypes.h"  // from @llvm-project
-#include "mlir/include/mlir/IR/MLIRContext.h"   // from @llvm-project
-#include "mlir/include/mlir/Support/LLVM.h"     // from @llvm-project
+#include "mlir/include/mlir/IR/MLIRContext.h"  // from @llvm-project
+#include "mlir/include/mlir/Support/LLVM.h"    // from @llvm-project
 
 namespace mlir {
 namespace heir {
@@ -21,26 +17,136 @@ namespace {
 using presburger::BoundType;
 using presburger::IntegerRelation;
 using presburger::VarKind;
+using testing::Eq;
+
+void debugFailure(const IntegerRelation &expected,
+                  const IntegerRelation &actual) {
+  llvm::outs() << "Expected:\n";
+  expected.print(llvm::outs());
+  llvm::outs() << "\nActual:\n";
+  actual.print(llvm::outs());
+
+  llvm::outs() << "Printing a sample point from expected:\n";
+  auto maybeSample = expected.findIntegerSample();
+  EXPECT_TRUE(maybeSample.has_value());
+  SmallVector<DynamicAPInt, 8> sample = maybeSample.value();
+
+  llvm::outs() << "Sample point in expected (with locals): \n";
+  for (auto &value : sample) {
+    value.print(llvm::outs());
+    llvm::outs() << ", ";
+  }
+  llvm::outs() << "\n";
+  llvm::outs().flush();
+
+  llvm::outs() << "Codegen for actual:\n";
+  auto result = generateLoopNestAsCStr(actual);
+  if (failed(result)) {
+    llvm::outs() << "Failed to generate code for actual relation.\n";
+  } else {
+    std::string actualCode = result.value();
+    llvm::outs() << actualCode << "\n";
+  }
+
+  llvm::outs() << "Codegen for expected:\n";
+  result = generateLoopNestAsCStr(expected);
+  if (failed(result)) {
+    llvm::outs() << "Failed to generate code for expected relation.\n";
+  } else {
+    std::string expectedCode = result.value();
+    llvm::outs() << expectedCode << "\n";
+  }
+
+  llvm::outs().flush();
+}
 
 TEST(UtilsTest, DiagonalLayout) {
-  // MLIRContext context;
+  MLIRContext context;
+  IntegerRelation fromVecLayout = relationFromString(
+      "(d, ct, slot) : "
+      "((d - slot) mod 8 == 0, "
+      "d >= 0, 7 >= d, slot >= 0, 15 >= slot, ct == 0)",
+      1, &context);
+  IntegerRelation toVecLayout = relationFromString(
+      "(d, ct, slot) : "
+      "((d - (slot + 3)) mod 8 == 0, "
+      "d >= 0, 7 >= d, slot >= 0, 15 >= slot, ct == 0)",
+      1, &context);
+  IntegerRelation matrixLayout = relationFromString(
+      "(row, col, ct, slot) : "
+      "((slot - row) mod 8 == 0, "
+      "(ct + slot - col) mod 8 == 0, "
+      "row >= 0, col >= 0, ct >= 0, slot >= 0, "
+      "15 >= slot, 7 >= ct, 7 >= row, 7 >= col)",
+      2, &context);
+  IntegerRelation expected = relationFromString(
+      "(row, col, ct, slot) : "
+      "(((slot + 3) - row) mod 8 == 0, "
+      "(ct + (slot + 3) - col) mod 8 == 0, "
+      "row >= 0, col >= 0, ct >= 0, slot >= 0, "
+      "15 >= slot, 7 >= ct, 7 >= row, 7 >= col)",
+      2, &context);
 
-  // // Diagonalize a 4x8 matrix into a 4x64 matrix.
-  // RankedTensorType matrixType =
-  //     RankedTensorType::get({4, 8}, IndexType::get(&context));
-  // RankedTensorType diagonalizedType =
-  //     RankedTensorType::get({4, 64}, IndexType::get(&context));
-  // IntegerRelation diagonalRelation =
-  //     getDiagonalLayoutRelation(matrixType, diagonalizedType);
+  IntegerRelation actual =
+      hoistConversionThroughMatvec(matrixLayout, fromVecLayout, toVecLayout);
 
-  // diagonalRelation.simplify();
-  // for (unsigned int i = 0; i < 4; ++i) {
-  //   for (unsigned int j = 0; j < 64; ++j) {
-  //     auto maybeExists =
-  //         diagonalRelation.containsPointNoLocal({j % 4, (i + j) % 8, i, j});
-  //     EXPECT_TRUE(maybeExists.has_value());
-  //   }
-  // }
+  auto actualVolume = actual.computeVolume();
+  auto expectedVolume = actual.computeVolume();
+
+  EXPECT_THAT(expectedVolume, Eq(actualVolume));
+
+  // Pick any value to check if it exists in the expected and actual relation.
+  auto maybeSample = actual.findIntegerSample();
+  EXPECT_TRUE(maybeSample.has_value());
+  SmallVector<DynamicAPInt, 8> sample = maybeSample.value();
+
+  llvm::outs() << "Sample point in actual (with locals): \n";
+  for (auto &value : sample) {
+    value.print(llvm::outs());
+    llvm::outs() << ", ";
+  }
+  llvm::outs() << "\n";
+  llvm::outs().flush();
+
+  // Copy first four values which gives domain/range values
+  SmallVector<DynamicAPInt, 4> point(sample.begin(), sample.begin() + 4);
+  auto maybeExists = expected.containsPointNoLocal(point);
+
+  if (!maybeExists.has_value()) {
+    llvm::outs() << "Printing toVec layout codegen for debugging:\n";
+    auto result = generateLoopNestAsCStr(toVecLayout);
+    if (failed(result)) {
+      llvm::outs() << "Failed to generate code for actual relation.\n";
+    } else {
+      std::string actualCode = result.value();
+      llvm::outs() << actualCode << "\n";
+    }
+    llvm::outs().flush();
+
+    debugFailure(expected, actual);
+    FAIL() << "Expected point not found in actual relation.";
+  }
+  EXPECT_TRUE(maybeExists.has_value());
+
+  // Spot check some points
+  std::vector<std::pair<int, int>> expectedDomain = {
+      {0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 3}, {4, 4}, {4, 5}, {4, 6},
+  };
+  std::vector<std::pair<int, int>> expectedRange = {
+      {0, 5}, {0, 6}, {0, 7}, {0, 0}, {7, 1}, {0, 1}, {1, 1}, {2, 1},
+  };
+
+  for (const auto &[domain, range] : llvm::zip(expectedDomain, expectedRange)) {
+    const auto &[row, col] = domain;
+    const auto &[ct, slot] = range;
+    auto maybeExists = actual.containsPointNoLocal({row, col, ct, slot});
+    if (!maybeExists.has_value()) {
+      llvm::outs() << "Failed to find point (" << row << ", " << col << ", "
+                   << ct << ", " << slot << ") in actual relation.\n";
+      debugFailure(expected, actual);
+      FAIL() << "Expected point not found in actual relation.";
+    }
+  }
 }
 
 }  // namespace
